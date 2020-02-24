f0f03439981b43376501f068d29ca650b95cf9f3
Annor-Akrofi <31506140+Annor-Akrofi@users.noreply.github.com>
2020-02-24 05:35:23 -0500

    Update 10682567_min_max.cpp

--- 10682567_min_max.cpp
+++ 10682567_min_max.cpp
@@ -24,3 +24,32 @@ int main ()
     cout << "The minimum number is : " << min;
     return 0;
 }
+
+////////////////////////////////////////////////////////////////////////
+//min and max search
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int arr[10], n, i, max, min;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    cout << "Enter the elements of the array : ";
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    max = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+    }
+    min = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+    cout << "The maximum numbere is : " << max;
+    cout << "The minimum number is : " << min;
+    return 0;
+}
